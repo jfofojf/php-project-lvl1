040331c1f38c1cc7c23463cefa9d8fb2341f167e
Igor <vvanow@yandex.ru>
2021-03-29 20:44:57 +0300

    x

--- src/games/prime.php
+++ src/games/prime.php
@@ -18,7 +18,7 @@ function isPrime(int $num): bool
     return $prime;
 }
 
-function start(): callable
+function start(): mixed
 {
     $generateGameData = function (): array {
         $question = rand(2, 100);
