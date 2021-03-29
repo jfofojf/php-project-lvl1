c4585d4c11f28b49a0f2770464416e5024f05176
Igor <vvanow@yandex.ru>
2021-03-29 23:47:41 +0300

    last fix

--- src/games/gcd.php
+++ src/games/gcd.php
@@ -6,13 +6,18 @@ use function BrainGames\Engine\runGame;
 
 const TASK = 'Find the greatest common divisor of given numbers.';
 
+function gcd($a, $b)
+{
+    return ($a % $b) ? gcd($b, $a % $b) : $b;
+}
+
 function start(): void
 {
     $generateGameData = function (): array {
         $firstDigit = rand(1, 30);
         $secondDigit = rand(1, 30);
         $question = "$firstDigit $secondDigit";
-        $correctAnswer = gmp_gcd($firstDigit, $secondDigit);
+        $correctAnswer = gcd($firstDigit, $secondDigit);
         return [$question, $correctAnswer];
     };
     runGame(TASK, $generateGameData);
